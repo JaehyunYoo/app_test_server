2b6c4f4a84a949312afe334a1cc0a05b32d63927
JaehyunYoo <nike361@naver.com>
2024-06-14 16:42:58 +0900

    test

--- app.js
+++ app.js
@@ -9,54 +9,24 @@ import fs from "fs";
 
 const app = express();
 
-const serviceAccount = JSON.parse(
-  await fs.promises.readFile("./service_account.json", "utf-8")
-);
-const scopes = ["https://www.googleapis.com/auth/androidpublisher"];
-const auth = new google.auth.GoogleAuth({
-  credentials: serviceAccount,
-  scopes: scopes,
-});
-
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.status(200).json({
-    state: "성공",
-  });
-});
-
-app.get("/verify-purchase", async (req, res) => {
-  const packageName = "com.vespexx.signal";
-  const subscriptionId = "product_coin"; // 구독 상품 ID
-  const purchaseToken =
-    "kamiglpmbnmdikgooegoadnk.AO-J1OymwztVS2a_nBVQtaTYXQgHPTJVkJ771caDyGAH6_UsUfBSQPNd2sL-4oh8HK5kZhOIYw19dbAVhQIgMd68tHDJz_47Tg";
-
-  try {
-    const authClient = await auth.getClient();
-    const accessToken = await authClient.getAccessToken();
-    const url = `https://androidpublisher.googleapis.com/androidpublisher/v3/applications/${packageName}/purchases/subscriptions/${subscriptionId}/tokens/${purchaseToken}`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken.token}`,
-      },
+app.post("/subscribe", async (req, res) => {
+    console.log("성공");
+    console.log(req.body);
+    res.json(200);
+    axios.post("https://api.revenuecat.com/v1/incoming-webhooks/apple-server-to-server-notification/WxNCrPrPjrmaINEwiuPMoHEQSuOYUcyC", req.body)
+    .then(response => {
+      // - Successfully forwarded to RevenueCat
+      console.log("Successfully forwarded to RevenueCat", response);
+    })
+    .catch(error => {
+      // - Consider a retry to RevenueCat if there's a network error or status code is 5xx
+      // - This is optional as RevenueCat should recheck the receipt within a few hours
+      console.error("Failed to send notification to RevenueCat", error);
     });
-    
-    console.log(response.data);
-    const purchaseData = response.data;
-    if (purchaseData.purchaseState === 0) {
-      res.status(200).send("Purchase token is valid");
-    } else {
-      res.status(400).send("Invalid purchase token");
-    }
-  } catch (error) {
-    console.error(
-      "Error verifying purchase token:",
-      error.response ? error.response.data : error.message
-    );
-    res.status(500).send("Failed to verify purchase token");
-  }
 });
+
 
 app.listen(8080, () => console.log("서버 온"));
 
